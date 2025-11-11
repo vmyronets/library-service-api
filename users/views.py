0270587f046318d6d756019a2edf2cbcb79b8cee
Volodymyr Myronets <volodymyrmyronets@gmail.com>
2025-11-11 18:11:02 +0200

    Create API views for user creation and management using DRF serializers

--- users/views.py
+++ users/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework import generics
 
-# Create your views here.
+from users.serializers import UserSerializer
+
+
+class CreateUserView(generics.CreateAPIView):
+    serializer_class = UserSerializer
+
+
+class ManageUserView(generics.RetrieveUpdateAPIView):
+    serializer_class = UserSerializer
+
+    def get_object(self):
+        return self.request.user
