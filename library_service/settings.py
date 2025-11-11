7e6ec3131cb969988ee22280f83ebc75d6bf55f8
Volodymyr Myronets <volodymyrmyronets@gmail.com>
2025-11-11 18:21:01 +0200

    Renew app name "Users"

--- library_service/settings.py
+++ library_service/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     "rest_framework_simplejwt",
     "rest_framework",
     "books",
-    "users",
+    "user",
 ]
 
 MIDDLEWARE = [
@@ -73,7 +73,7 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'library_service.wsgi.application'
 
-AUTH_USER_MODEL = "users.User"
+AUTH_USER_MODEL = "user.User"
 
 
 # Database
