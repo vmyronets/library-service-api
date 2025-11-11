21f9b9287d70ac1b31ec5973894d22133b6b253b
Volodymyr Myronets <volodymyrmyronets@gmail.com>
2025-11-11 15:25:25 +0200

    Add custom `User` model with email authentication and update settings

--- library_service/settings.py
+++ library_service/settings.py
@@ -73,6 +73,8 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'library_service.wsgi.application'
 
+AUTH_USER_MODEL = "users.User"
+
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
