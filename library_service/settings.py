9c950f4c9145ff82d6a6cd44c9880c8067934918
Volodymyr Myronets <volodymyrmyronets@gmail.com>
2025-11-19 17:07:42 +0200

    Add request throttling and simple_jwt configuration to Django REST framework settings.

--- library_service/settings.py
+++ library_service/settings.py
@@ -143,7 +143,12 @@ TELEGRAM_CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": [
         "rest_framework_simplejwt.authentication.JWTAuthentication",
-    ]
+    ],
+    "DEFAULT_THROTTLE_CLASSES": [
+        "rest_framework.throttling.AnonRateThrottle",
+        "rest_framework.throttling.UserRateThrottle",
+    ],
+    "DEFAULT_THROTTLE_RATES": {"anon": "1000/day", "user": "1000/day"},
 }
 
 SIMPLE_JWT = {
