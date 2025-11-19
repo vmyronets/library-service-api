c7d267cb1b6806c69c818464916b53bf3cd99a14
Volodymyr Myronets <volodymyrmyronets@gmail.com>
2025-11-19 18:06:42 +0200

    Restrict book management permissions to admin users and allow read-only access for others.

--- books/views.py
+++ books/views.py
@@ -1,4 +1,9 @@
 from rest_framework import viewsets
+from rest_framework.permissions import (
+    IsAuthenticatedOrReadOnly,
+    IsAdminUser,
+    SAFE_METHODS
+)
 from books.models import Book
 from books.serializers import BookSerializer
 
@@ -6,3 +11,10 @@ from books.serializers import BookSerializer
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+
+    def get_permissions(self):
+        """Only admin users can create, update or delete books."""
+        if self.action in ("list", "retrieve"):
+            return [IsAuthenticatedOrReadOnly()]
+
+        return [IsAdminUser()]
