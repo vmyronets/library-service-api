284f1b4864fbc1986cda5559d2e398c4c6de7a45
Volodymyr Myronets <volodymyrmyronets@gmail.com>
2025-11-20 21:02:58 +0200

    Enhance `BorrowingAdmin` with additional fields in `list_display` and updated `ordering`.

--- borrowings/admin.py
+++ borrowings/admin.py
@@ -5,7 +5,14 @@ from borrowings.models import Borrowing
 
 @admin.register(Borrowing)
 class BorrowingAdmin(admin.ModelAdmin):
-    list_display = ("id", "book", "user", "expected_return_date")
+    list_display = (
+        "id",
+        "book",
+        "user",
+        "borrow_date",
+        "expected_return_date",
+        "is_active"
+    )
     list_filter = ("book", "user")
     search_fields =("book__title", "user__is_active")
     ordering = ("-borrow_date",)
